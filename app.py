f3c81f22f3e0d175a87040349fa2671060ef6c24
nmbrouss <nicolebroussard@Nicoles-MacBook-Air.local>
2021-05-04 18:51:56 -0400

    Moved a copy of sqlite main folder for easy use and created route list

---
+++ app.py
@@ -0,0 +1,49 @@
+import numpy as np
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+
+
+# Database Setup
+engine = create_engine("sqlite:///titanic.sqlite")
+
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(engine, reflect=True)
+
+# Save reference to the table
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+#Create our session from Python to the DB
+session = Session(engine)
+
+# Flask Setup
+app = Flask(__name__)
+
+#Flask Routes
+@app.route("/")
+def home():
+    """List all available api routes."""
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/<start><br/>"
+        f"/api/v1.0/<start>/<end>"
+    )
+
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    ""Let's look into the precipitation data.""
+    precip_results = session.query(Measurement.prcp, ).all()
+
+
+
+
