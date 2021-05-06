40e64437b7d672b8671fa75c0130d6a4cd595ba2
nmbrouss <nicolebroussard@Nicoles-MacBook-Air.local>
2021-05-06 19:35:48 -0400

    added json lists to precipitation and station

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import numpy as np
+import datetime as dt
+import pandas as pd
 
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
@@ -14,7 +16,7 @@ engine = create_engine("sqlite:///../Resoucres/hawaii.sqlite", echo=False)
 # reflect an existing database into a new model
 Base = automap_base()
 # reflect the tables
-Base.prepare(engine, reflect=True)
+Base.prepare(engine)
 
 # Save reference to the table
 Measurement = Base.classes.measurement
@@ -41,8 +43,8 @@ def home():
         f"/api/v1.0/precipitation<br/>"
         f"/api/v1.0/stations<br/>"
         f"/api/v1.0/tobs<br/>"
-        f"/api/v1.0/<start><br/>"
-        f"/api/v1.0/<start>/<end>"
+        f"/api/v1.0/start<br/>"
+        f"/api/v1.0/start/end<br/>"
     )
 
 @app.route("/api/v1.0/precipitation")
@@ -50,11 +52,12 @@ def precipitation():
     """Lets look into the precipitation data."""
     #Query last 12 months prcp data
     precip_results = session.query(Measurement.date, Measurement.prcp).filter(Measurement.date >= year_earlier_date).group_by(Measurement.date).all()
+    prcp_dict = []
     for date, prcp in precip_results:
-        prcp_dict = {}
-        prcp_dict["date"] = date
-        prcp_dict["prcp"] = prcp
-
+        prcp_data = {}
+        prcp_data["date"] = date
+        prcp_data["prcp"] = prcp
+        prcp_dict.append(prcp_data)
     return jsonify(prcp_dict)
 
 @app.route("/api/v1.0/stations")
@@ -64,5 +67,14 @@ def station():
 
 @app.route("/api/v1.0/tobs")
 def tobs():
-    tobs_results = session.query(Measurement.date, Measurement.tobs).filter(Measurement.date >= year_earlier_date).filter(Measurement.station == most_active_stid).all()
-    return jsonify(tobs_results) 
+    tobs_results = session.query(Measurement.date, Measurement.tobs).filter(Measurement.date >= year_earlier_date).filter(Measurement.station == most_active_stid).order_by(Measurement.tobs).all()
+    tobs_dict = []
+    for date, tobs in tobs_results:
+        tobs_data = {}
+        tobs_data["date"] = date
+        tobs_data["tobs"] = tobs
+        tobs_dict.append(tobs_data)
+    return jsonify(tobs_dict) 
+
+if __name__ == "__main__":
+    app.run(debug=True)
